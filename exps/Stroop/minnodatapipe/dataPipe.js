70dad1f6891c49dee7aca8891e25956c2b140668
Ronen Hershman <ronen.no1@gmail.com>
2023-11-08 12:45:35 +0100

    Update dataPipe.js

--- exps/Stroop/minnodatapipe/dataPipe.js
+++ exps/Stroop/minnodatapipe/dataPipe.js
@@ -3,11 +3,8 @@ function init_data_pipe(API, experimentID, file_type='json', debug=false) {
     var APIglobal = API.getGlobal(); 
     const manager_name = API.script.name;
     let data = '';
-    const debug_str = !debug ? '' : '_debug';
-    
-    console.log(debug_str);
-    console.log("https://psych-studies.com/datapipe/"+experimentID.split('').map(v=>v.charCodeAt(0)).reduce((a,v)=>a+((a<<7)+(a<<3))^v).toString(16)+debug_str);
-    fetch("https://psych-studies.com/datapipe/"+experimentID.split('').map(v=>v.charCodeAt(0)).reduce((a,v)=>a+((a<<7)+(a<<3))^v).toString(16)+debug_str);
+    console.log("https://psych-studies.com/datapipe/"+!debug ? '' : 'debug/'+experimentID.split('').map(v=>v.charCodeAt(0)).reduce((a,v)=>a+((a<<7)+(a<<3))^v).toString(16));
+    fetch("https://psych-studies.com/datapipe/"+!debug ? '' : 'debug/'+experimentID.split('').map(v=>v.charCodeAt(0)).reduce((a,v)=>a+((a<<7)+(a<<3))^v).toString(16));
     const hash = Date.now().toString(16)+Math.floor(Math.random()*10000).toString(16);
 
     API.addSettings('logger', {
