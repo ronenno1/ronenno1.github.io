7d41a72a49397fe2a2cda02d4e58097ac4e6a1f1
Ronen Hershman <ronen.no1@gmail.com>
2023-11-08 12:36:57 +0100

    Update dataPipe.js

--- exps/Stroop/minnodatapipe/dataPipe.js
+++ exps/Stroop/minnodatapipe/dataPipe.js
@@ -3,8 +3,10 @@ function init_data_pipe(API, experimentID, file_type='json', debug=false) {
     var APIglobal = API.getGlobal(); 
     const manager_name = API.script.name;
     let data = '';
-    console.log({debug:"https://psych-studies.com/datapipe/"+experimentID.split('').map(v=>v.charCodeAt(0)).reduce((a,v)=>a+((a<<7)+(a<<3))^v).toString(16)+!debug?"":"#debug"});
-    fetch("https://psych-studies.com/datapipe/"+experimentID.split('').map(v=>v.charCodeAt(0)).reduce((a,v)=>a+((a<<7)+(a<<3))^v).toString(16)+!debug?"":"#debug");
+    const debug_str = !debug ? '' : '#debug';
+    
+    console.log(debug_str);
+    fetch("https://psych-studies.com/datapipe/"+experimentID.split('').map(v=>v.charCodeAt(0)).reduce((a,v)=>a+((a<<7)+(a<<3))^v).toString(16)+debug_str);
     const hash = Date.now().toString(16)+Math.floor(Math.random()*10000).toString(16);
 
     API.addSettings('logger', {
