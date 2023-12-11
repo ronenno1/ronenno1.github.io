40d2a943aed266de351f79165b98a22a906a2963
Ronen Hershman <ronen.no1@gmail.com>
2023-12-11 17:07:51 +0100

    Update Runner.js

--- exps/Stroop/de/Runner.js
+++ exps/Stroop/de/Runner.js
@@ -1,172 +1,258 @@
-define(['managerAPI'], function(Manager){
-
-// 'minno_mesh.js',
-// 'https://cdn.jsdelivr.net/npm/@mediapipe/camera_utils/camera_utils.js',
-// 'https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/face_mesh.js'], function(Manager, minno_mesh, facemesh, facemesh2){
-
-// 'https://cdn.jsdelivr.net/gh/minnojs/minno-datapipe/datapipe.min.js'
-
-    const url = window.location.href;
-    const url_parts = url.split('/');
-    const next_url = url_parts[0]+'//'+url_parts[2]+'/'+url_parts[3]+'/971ef4544bd1f7d60a1e8aa4fa18193c2b3d95f4/'+url_parts[5]+'/'
-    console.log(next_url);
-    
+define(['managerAPI', 'https://cdn.jsdelivr.net/gh/minnojs/minno-datapipe/datapipe.min.js'], function(Manager){
 
 	var API    = new Manager();
+    API.addSettings('rtl', true);
+    // API.setName('taskNameTest');
   //  init_data_pipe(API, 'qH8i5g1AMmBd', 'csv', true);
+    // init_data_pipe(API, 'qH8i5g1AMmBd', 'csv');
+    // var APIglobal = API.getGlobal(); 
 
-    var instStyle = "font-size:20px; text-align:middle;  margin-right:10px; font-family:arial";
+    // API.addSettings('logger', {
+    //     // gather logs in array
+    //     onRow: function(logName, log, settings, ctx){
+    //         if (logName==='anonymous manager')
+    //     {
+    //             return;
+    //     }
+    //         if (!ctx.logs) ctx.logs = [];
+    //         ctx.logs.push(log);
+    //     },
+    //     // onEnd trigger save (by returning a value)
+    //     onEnd: function(name, settings, ctx){
     
-    var global = API.getGlobal(); 
+    //         return ctx.logs;
+    //     },
+    //     // Transform logs into a string
+    //     // we save as CSV because qualtrics limits to 20K characters and this is more efficient.
+    //     serialize: function (logName, logs, settings, ctx) {
+    //         // console.log({logName, logs, settings, ctx})
+    //         return JSON.stringify(logs);
+    //     },
+    //     // Set logs into an input (i.e. put them wherever you want)
+    //     send: function(logName, serialized, settings, ctx){
+    //         // console.log({logName, serialized});
+    //         if (logName===API.script.sequence[API.script.sequence.length-1].inherit)
+    //         	return	fetch("https://pipe.jspsych.org/api/data/", {
+    //                   method: "POST",
+    //                   headers: {
+    //                     "Content-Type": "application/json",
+    //                     Accept: "*/*",
+    //                   },
+    //                   body: JSON.stringify({
+    //                     experimentID: "qH8i5g1AMmBd",
+    //                     filename: APIglobal.$postOnce.sessionId+".txt",
+    //                     data: serialized
+    //                   }),
+    //                 });
+    //     }
+    // });
+
     
-    var stims = ['imgs/Stroop/1_1.bmp', 'imgs/Stroop/1_2.bmp', 'imgs/Stroop/2_1.bmp', 'imgs/Stroop/2_2.bmp', 'imgs/Stroop/3_1.bmp', 'imgs/Stroop/3_2.bmp'];
+    // API.addSettings("logger", {
+    // 	pulse: 0,
+    // 	url: '',	
+    // 	logfn: function(log,pagesData, global){
+    // 		return 
+    		
+    // 		fetch("https://pipe.jspsych.org/api/data/", {
+    //           method: "POST",
+    //           headers: {
+    //             "Content-Type": "application/json",
+    //             Accept: "*/*",
+    //           },
+    //           body: JSON.stringify({
+
+    //             experimentID: "qH8i5g1AMmBd",
+    //             filename: "3.csv",
+    //             data: log
+    //           }),
+    //         });
+    // 		;
+    // 	}
+    // });
+
     
-    API.addSettings('preloadImages', stims);
-	console.log('test');
+    // API.addSettings('logger', {
+    //     pulse: 300,
+    //     // gather logs in array
+    //         onRow: function(logName, log, settings, ctx){
+    //             if (!ctx.logs) ctx.logs = [];
+    //             ctx.logs.push(log);
+    //         },
+    //         // onEnd trigger save (by returning a value)
+    //         onEnd: function(name, settings, ctx){
+    //             console.log(name, settings)
+    //             // const parsedData = JSON.parse(serialized);
+    //             // const jsonKeys = Object.keys(parsedData[0]);
+    //             // const headerData = jsonKeys.join(',');
+    //             // const rowData = parsedData.map((item) => {
+    //             //     return jsonKeys.map((key) => item[key]).join(',');
+    //             // });
+    //             // const csv = `${headerData}\n${rowData.join('\n')}`;
+    
+    //             return fetch("https://pipe.jspsych.org/api/data/", {
+    //               method: "POST",
+    //               headers: {
+    //                 "Content-Type": "application/json",
+    //                 Accept: "*/*",
+    //               },
+    //               body: JSON.stringify({
+    
+    //                 experimentID: "qH8i5g1AMmBd",
+    //                 filename: name+"4.txt",
+    //                 data: JSON.stringify(ctx.logs)
+    //               }),
+    //             });
 
-//    global.init_minno_mesh = init_minno_mesh;
-		console.log('test2');
+    //         },
+    //         // Transform logs into a string
+    //         serialize: function(name, logs, settings){
+    //             return JSON.stringify(logs);
+    //         },
 
-    global.keys =  API.shuffle(['i', 'e']),
+    //     send: function(name, serialized, settings, ctx){
+            
+    //     }
+    // });
 
-    global.mins = 10;
-    //global.detectionCycleDuration = 90;
-    global.kickout = false; //Should we kick them out due to error responses?
+    var images2preload = [];
 
-    // init_minno_faces(global, faceapi); // This function load minno_faces components
+    var number = 0;
+    var color_1 = 0;
+    var color_2 = 0;
+    var color_3 = 0;
+    var color_4 = 0;
+    
+    var image_url = 'https://psych-studies.com/users/rhershman/dataPipe-62/v1/digits/';
 
-    //global.enoughTime = Math.random() > 0.5 ? 300 : 450;
-    global.enoughTime = 200;
-    global.get_times = get_times;
-    function get_times(){
-        return global.time_n - global.time_n_1;
+    for (number = 1; number <= 4; number+=1) {
+        images2preload.push(image_url+number+'d.png');
+        images2preload.push(image_url+number+'r.png');
+        images2preload.push(image_url+number+'t.png');
     }
     
-    var noticeInst = '<b>Am unteren Rand des Bildschirmes sehen sie Ihre Webcamaufnahme. <br>' + 
-        'Ihr Gesicht sollte sich in der Mitte des Rechteckes befinden, damit das Programm Ihre Augen erkennen kann. <br></br>' + 
-        '<u>Wenn der Rahmen grün erscheint werden Ihre Augen erkannt. Wenn der Rahmen Rot erscheint, kann das Programm Ihre Augen nicht erkennen.<br><br>'+
-        'Bitte stellen Sie sicher, dass sich nur eine Person (Sie) während dem Experiment vor dem Bildschirm befindet. </u>.<br><br>';
+    
+    
+    
+     
+     
+    // for (color_1 = 1; color_1 <= 4; color_1++) {
+    //     images2preload.push('neutral_digits2/1_1_'+color_1+'_'+color_1+'_'+color_1+'_'+color_1+'.png');
+
+    // }
 
 
-     var MainInst = '<p>Ihre Aufgabe ist es, so rasch als möglich zu entscheiden, welche Farbe das gezeigte Rechteck oder Wort hat.</br></p>'+
-                    '<p>Wenn die Farbe <b>ROT</b> ist, drücken Sie bitte die <b>'+global.keys[0].toUpperCase()+'-Taste</b></p>'+
-                    '<p>Wenn die Farbe <b>BLAU</b> ist, drücken Sie bitte die <b>'+global.keys[1].toUpperCase()+'-Taste</b></p></br>'+
+    // for (color_1 = 1; color_1 <= 4; color_1++){
+    //     for (color_2 = 1; color_2 <= 4; color_2++){
+    //         if (color_1==color_2)
+    //             continue;
+    //         images2preload.push('neutral_digits2/2_1_'+color_1+'_'+color_1+'_'+color_2+'_'+color_2+'.png');
+    //     }
+    // }
+    // for (color_1 = 1; color_1 <= 4; color_1++){
+    //     for (color_2 = 1; color_2 <= 4; color_2++){
+    //         for (color_3 = 1; color_3 <= 4; color_3++){
+    //                 if ([color_1, color_2, color_3].filter((x, i, a) => a.indexOf(x) == i).length<3)
+    //                     continue;
+    //                 images2preload.push('neutral_digits2/3_1_'+color_1+'_'+color_2+'_'+color_3+'.png');
+    //         }
+    //     }
+    // }
+    
+    // for (color_1 = 1; color_1 <= 4; color_1++){
+    //     for (color_2 = 1; color_2 <= 4; color_2++){
+    //         for ( color_3 = 1; color_3 <= 4; color_3++){
+    //             for (color_4 = 1; color_4 <= 4; color_4++){
+    //                 if ([color_1, color_2, color_3, color_4].filter((x, i, a) => a.indexOf(x) == i).length<4)
+    //                     continue;
+    //                 images2preload.push('neutral_digits2/4_1_'+color_1+'_'+color_2+'_'+color_3+'_'+color_4+'.png');
+    //             }
+    //         }
+    //     }
+    // }
 
-                    '<p style="font-weight: bold;">Bitte schauen Sie während der Dauer des gesamten Experiments auf die Mitte des Bildschirms.</p></br>'+
+    // list = list.filter((x, i, a) => a.indexOf(x) == i)
 
-                    '<p>Legen Sie die Finger nun auf die Tasten '+global.keys[0].toUpperCase()+', und '+global.keys[1].toUpperCase()+'.</p>'+
-                     '<br/>' + 
-	                noticeInst ;
 
+
+        
+    API.addSettings('preloadImages', images2preload);
+
+    
 	API.addTasksSet(
 	{
+        realstart: [{
+            type: 'message',
+            buttonText: 'המשך',
 
+            name: 'realstart',
+            templateUrl: 'realstart.jst',
+            title: 'טופס הסכמה',
+            piTemplate: false,
+            header: 'ברוכים הבאים לניסוי'
+        }],
+
+        images: [{
+            type: 'message',
+            buttonText: 'המשך',
+
+            name: 'images',
+            templateUrl: 'images.jst',
+            title: 'Objects',
+            piTemplate: false
+        }],
         
 	    subject : 
 		[{
-			type: 'quest', piTemplate: true, name: 'subject', scriptUrl: 'subject.js'
+			type: 'quest', piTemplate: true, name: 'subject', scriptUrl: 'subject.js', log:false
 		}],
-
-
-        end: [{
-            type: 'message',
-
-            name: 'end',
-            templateUrl: 'end.jst',
-            title: 'The End',
-            piTemplate: true,
-            header: 'The End', 
-            buttonText: 'Back to Prolific'
-        }],
-
-        consent: [{
-            type: 'message',
-
-            name: 'consent',
-            templateUrl: 'consent.jst',
-            title: 'Consent Form',
-            header: 'Danke für Ihre Teilnahme an diesem Experiment!',
-            buttonText: 'Fortfahren',
-
-        }],
-
-        commit: [{
-            type: 'message',
-            //buttonText: 'continue',
-
-            name: 'commit',
-            templateUrl: 'commit.jst',
-            title: 'Commit',
-            header: 'Wichtige Anmerkung!', 
-            buttonText: 'Fortfahren'
-        }],
-
+		prac :
+		[{
+			type: 'time', name: 'prac', scriptUrl: 'prac.js'
+		}],
+		welcome :
+		[{
+			type: 'time', name: 'welcome', scriptUrl: 'insts_v4.js?v=welcome', current: {type:'welcome'}
+		}],
 
 		Stroop :
 		[{
-			type: 'time', name: 'Stroop', scriptUrl: 'Stroop_EBR.js' , 
-			current: {
-			    myID:'stroop',
-			    maxTimeoutsInBlock:8,
-			    maxFailedBlocks:2,
-			    num_of_prac_trials:4, // 4
-                minScore4exp: 8,      // 8
-			    num_of_trials:60,     // 60
-			    blockInst: [
-			            
-    	            '<div style="'+instStyle+'"><color="FFFFFF">' +
-                        '<p>In diesem Teil des Experimentes sehen Sie Farben.</p>'+
-                            MainInst+ 
-                        '<p>Drücken Sie die Leertaste, um den Übungslauf zu beginnen.</p>'+
-                        
-                    '</div>',
-			            
-			        ////////////////////////////////////////////////////////
-		            '<div style="'+instStyle+'"><color="FFFFFF">' +
-		                '<p><b>Der Übungsteil ist geschafft!</b></p>'+
-                        '<p>Zur Erinnerung: In diesem Teil des Experimentes sehen Sie Farben.</p>'+
-                            MainInst+ 
-                        '<p>Drücken Sie die Leertaste, um mit dem Experiment zu beginnen.</p>'+
-                    '</div>',
-			        ////////////////////////////////////////////////////////
-			        '<div style="'+instStyle+'"><color="FFFFFF">' +
-    	                '<p><b>Zeit für eine kurze Pause!</b></p>'+
-                        '<p>Zur Erinnerung: In diesem Teil des Experimentes sehen Sie Farben.</p>'+
-                            MainInst+ 
-                        '<p>Drücken Sie die Leertaste, um fortzufahren.</p>'+
-                        
-                    '</div>',
-			        ////////////////////////////////////////////////////////
-			        '<div style="'+instStyle+'"><color="FFFFFF">' +
-    	                '<p><b>Das war nicht ganz richtig. Starten wir einen neuen Versuch.</b></p>'+
-                        '<p>Zur Erinnerung: In diesem Teil des Experimentes sehen Sie Farben.</p>'+
-                            MainInst+ 
-                        '<p>Drücken Sie die Leertaste, um den Übungslauf neu zu beginnen.</p>'+
-                    '</div>'
-		        ]
-			}
+			type: 'time', name: 'Stroop', scriptUrl: 'Stroop.js' 
+		}],
+	
+		debriefing : [{
+			type: 'quest', piTemplate: true, name: 'debriefing', scriptUrl: 'debriefing.js' 
+
+		}],
+		rest :
+		[{
+			type: 'time', name: 'rest', scriptUrl: 'insts_v4.js?v=rest', current: {type:'rest'}
+		}],
+		deb :
+		[{
+			type: 'time', name: 'deb', scriptUrl: 'insts_v4.js?v=deb', current: {type:'deb'}
 		}],
 
+		end :
+		[{
+			type: 'time', name: 'end', scriptUrl: 'insts_v4.js?v=end', current: {type:'end'}
+		}]
 
-        redirect:
-        [{ 
 
-            type:'redirect', name:'redirecting', url    : next_url+'?oldsid='+global.$postOnce.sessionId
-        }],
 	});
 
     //define the sequence of the study
     API.addSequence([
-        {inherit: 'consent'},
+         {inherit: 'realstart'},
+         {inherit: 'subject'},
+         {inherit: 'welcome'},
 
-      //  {inherit: 'commit'},
-      //  {inherit: 'subject'},
+        //{inherit: 'Stroop'},
+         {inherit: 'deb'},
 
-
-       // {inherit: 'Stroop'},
-
-       // {inherit: 'redirect'}
-        
+ {inherit: 'debriefing'},
+         {inherit: 'end'}
 	]);
+	
 	return API.script;
 });
