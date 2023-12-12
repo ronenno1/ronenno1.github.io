4354c8492a605154aab245d3d47ed41bc5dad26a
Ronen Hershman <ronen.no1@gmail.com>
2023-12-12 09:16:49 +0100

    Update Runner.js

--- exps/Stroop/de/Runner.js
+++ exps/Stroop/de/Runner.js
@@ -1,8 +1,41 @@
-define(['managerAPI', 'https://cdn.jsdelivr.net/gh/minnojs/minno-datapipe/datapipe.min.js'], function(Manager){
+define(['managerAPI',
+'minno_mesh.js',
+'https://cdn.jsdelivr.net/npm/@mediapipe/camera_utils/camera_utils.js',
+'https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/face_mesh.js'], function(Manager, minno_mesh, facemesh, facemesh2){
+
+
+    const url = window.location.href;
+    const url_parts = url.split('/');
+    const next_url = url_parts[0]+'//'+url_parts[2]+'/'+url_parts[3]+'/971ef4544bd1f7d60a1e8aa4fa18193c2b3d95f4/'+url_parts[5]+'/'
+    console.log(next_url);
+    
 
 	var API    = new Manager();
-    API.addSettings('rtl', true);
-  var noticeInst = '<b>Am unteren Rand des Bildschirmes sehen sie Ihre Webcamaufnahme. <br>' + 
+
+    var instStyle = "font-size:20px; text-align:middle;  margin-right:10px; font-family:arial";
+    
+    var global = API.getGlobal(); 
+    
+    var stims = ['imgs/Stroop/1_1.bmp', 'imgs/Stroop/1_2.bmp', 'imgs/Stroop/2_1.bmp', 'imgs/Stroop/2_2.bmp', 'imgs/Stroop/3_1.bmp', 'imgs/Stroop/3_2.bmp'];
+    
+    API.addSettings('preloadImages', stims);
+    global.init_minno_mesh = init_minno_mesh;
+    global.keys =  API.shuffle(['i', 'e']),
+
+    global.mins = 10;
+    //global.detectionCycleDuration = 90;
+    global.kickout = false; //Should we kick them out due to error responses?
+
+    // init_minno_faces(global, faceapi); // This function load minno_faces components
+
+    //global.enoughTime = Math.random() > 0.5 ? 300 : 450;
+    global.enoughTime = 200;
+    global.get_times = get_times;
+    function get_times(){
+        return global.time_n - global.time_n_1;
+    }
+    
+    var noticeInst = '<b>Am unteren Rand des Bildschirmes sehen sie Ihre Webcamaufnahme. <br>' + 
         'Ihr Gesicht sollte sich in der Mitte des Rechteckes befinden, damit das Programm Ihre Augen erkennen kann. <br></br>' + 
         '<u>Wenn der Rahmen grün erscheint werden Ihre Augen erkannt. Wenn der Rahmen Rot erscheint, kann das Programm Ihre Augen nicht erkennen.<br><br>'+
         'Bitte stellen Sie sicher, dass sich nur eine Person (Sie) während dem Experiment vor dem Bildschirm befindet. </u>.<br><br>';
@@ -22,23 +55,16 @@ define(['managerAPI', 'https://cdn.jsdelivr.net/gh/minnojs/minno-datapipe/datapi
 	{
 
         
-	    subject : 
-		[{
-			type: 'quest', piTemplate: true, name: 'subject', scriptUrl: 'subject.js'
-		}],
-
-
-        end: [{
+	commit: [{
             type: 'message',
+            //buttonText: 'continue',
 
-            name: 'end',
-            templateUrl: 'end.jst',
-            title: 'The End',
-            piTemplate: true,
-            header: 'The End', 
-            buttonText: 'Back to Prolific'
+            name: 'commit',
+            templateUrl: 'commit.jst',
+            title: 'Commit',
+            header: 'Wichtige Anmerkung!', 
+            buttonText: 'Fortfahren'
         }],
-
         consent: [{
             type: 'message',
 
@@ -49,20 +75,13 @@ define(['managerAPI', 'https://cdn.jsdelivr.net/gh/minnojs/minno-datapipe/datapi
             buttonText: 'Fortfahren',
 
         }],
+        subject : 
+		[{
+			type: 'quest', piTemplate: true, name: 'subject', scriptUrl: 'subject.js'
+		}],        
+		
 
-        commit: [{
-            type: 'message',
-            //buttonText: 'continue',
-
-            name: 'commit',
-            templateUrl: 'commit.jst',
-            title: 'Commit',
-            header: 'Wichtige Anmerkung!', 
-            buttonText: 'Fortfahren'
-        }],
-
-
-		Stroop :
+        Stroop :
 		[{
 			type: 'time', name: 'Stroop', scriptUrl: 'Stroop_EBR.js' , 
 			current: {
@@ -105,7 +124,15 @@ define(['managerAPI', 'https://cdn.jsdelivr.net/gh/minnojs/minno-datapipe/datapi
                     '</div>'
 		        ]
 			}
+			
 		}],
+
+        
+/*
+
+
+
+		
 
 
         redirect:
@@ -113,20 +140,22 @@ define(['managerAPI', 'https://cdn.jsdelivr.net/gh/minnojs/minno-datapipe/datapi
 
             type:'redirect', name:'redirecting', url    : next_url+'?oldsid='+global.$postOnce.sessionId
         }],
+        */
 	});
 
     //define the sequence of the study
     API.addSequence([
-        {inherit: 'consent'},
+//        {inherit: 'consent'},
 
-      //  {inherit: 'commit'},
-      //  {inherit: 'subject'},
+  //      {inherit: 'commit'},
+    //    {inherit: 'subject'},
 
 
-       // {inherit: 'Stroop'},
+        {inherit: 'Stroop'},
 
-       // {inherit: 'redirect'}
+//        {inherit: 'redirect'}
         
 	]);
+	
 	return API.script;
 });
